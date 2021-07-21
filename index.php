09661341bd96218566a79045dd3f574040bc7089
Felipe Dias <felipedias7@msn.com>
2021-07-21 11:27:01 -0300

    changeIndex

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ use PHPHtmlParser\Dom;
 $dom = new Dom;
 $dom->loadFromUrl('https://wwwapps.ups.com/WebTracking/processInputRequest?HTMLVersion=5.0&loc=en_US&Requester=UPSHome&tracknum=1Z29994X0340447519
 &AgreeToTermsAndConditions=yes&ignore=&track.x=24&track.y=14/trackdetails');
-$html = $dom->outerHtml;
+$html = $dom->outerHtmlxxx;
 
 // or
 // $dom->loadFromUrl('https://wwwapps.ups.com/WebTracking/processInputRequest?HTMLVersion=5.0&loc=en_US&Requester=UPSHome&tracknum=1Z29994X0340447519
