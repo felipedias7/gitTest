616e896aee898dadaf919a8f44348fd24b88718f
Felipe Dias <felipedias7@msn.com>
2021-07-21 11:35:01 -0300

    test

--- index.php
+++ index.php
@@ -2,16 +2,18 @@
 require "vendor/autoload.php";
 use PHPHtmlParser\Dom;
 
-$dom = new Dom;
-$dom->loadFromUrl('https://wwwapps.ups.com/WebTracking/processInputRequest?HTMLVersion=5.0&loc=en_US&Requester=UPSHome&tracknum=1Z29994X0340447519
-&AgreeToTermsAndConditions=yes&ignore=&track.x=24&track.y=14/trackdetails');
-$html = $dom->outerHtmlxxx;
-
-// or
+// $dom = new Dom;
 // $dom->loadFromUrl('https://wwwapps.ups.com/WebTracking/processInputRequest?HTMLVersion=5.0&loc=en_US&Requester=UPSHome&tracknum=1Z29994X0340447519
 // &AgreeToTermsAndConditions=yes&ignore=&track.x=24&track.y=14/trackdetails');
-// $html = $dom->outerHtml; // same result as the first example
+// $html = $dom->outerHtml;
 
-echo $html;
+// // or
+// // $dom->loadFromUrl('https://wwwapps.ups.com/WebTracking/processInputRequest?HTMLVersion=5.0&loc=en_US&Requester=UPSHome&tracknum=1Z29994X0340447519
+// // &AgreeToTermsAndConditions=yes&ignore=&track.x=24&track.y=14/trackdetails');
+// // $html = $dom->outerHtml; // same result as the first example
+
+// echo $html;
 
 ?>
+
+Hello World!
